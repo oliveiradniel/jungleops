37cce387fd6cc0943e3e2b5b17f4f1e787fdcec3
oliveiradniel <kadadniel@gmail.com>
2025-12-13 12:48:39 -0300

    feat(web): add field name faceted filters to update table

--- apps/web/src/view/pages/task-audit-logs/tables/update-table/index.tsx
+++ apps/web/src/view/pages/task-audit-logs/tables/update-table/index.tsx
@@ -1,12 +1,17 @@
 import { useListTaskUpdateAuditLogQuery } from '@/app/hooks/queries/use-list-task-update-audit-log-query';
 import { useColumns } from './use-columns';
 
+import { fieldLabels } from '@/config/labels';
+
 import { DataTable } from '@/view/components/data-table';
 import { DataTableColumnsVisibilityDropdown } from '@/view/components/data-table/data-table-columns-visibility-dropdown';
 import { DataTableContent } from '@/view/components/data-table/data-table-content';
 import { DataTableFallback } from '@/view/components/data-table/data-table-fallback';
 import { EmptyLog } from './empty-log';
 import { DataTableTextFilter } from '@/view/components/data-table/data-table-text-filter';
+import { DataTableFacetedFilter } from '@/view/components/data-table/data-table-faceted-filter';
+
+import type { FieldName } from '@challenge/shared';
 
 export function TaskUpdateAuditLogTable() {
   const { taskUpdateAuditLogsList, isTaskUpdateAuditLogsLoading } =
@@ -37,7 +42,15 @@ export function TaskUpdateAuditLogTable() {
           <div className="flex items-center gap-4">
             <DataTableTextFilter placeholder="Procure por autor, título, campo ou valores" />
 
-            <DataTableColumnsVisibilityDropdown />
+            <div className="flex items-center gap-1">
+              <DataTableFacetedFilter<FieldName>
+                placeholder="Campo"
+                column="fieldName"
+                labels={fieldLabels}
+              />
+
+              <DataTableColumnsVisibilityDropdown />
+            </div>
           </div>
         )}
 
