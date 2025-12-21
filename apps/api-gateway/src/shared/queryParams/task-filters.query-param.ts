46377a2ba63eb5b4b6f405bf42507bd1df4be640
oliveiradniel <kadadniel@gmail.com>
2025-12-21 19:45:03 -0300

    feat(api-gateway): add TaskFiltersQueryParam DTO

---
+++ apps/api-gateway/src/shared/queryParams/task-filters.query-param.ts
@@ -0,0 +1,58 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+import { Transform } from 'class-transformer';
+import { IsNumber, IsOptional, IsString, Min } from 'class-validator';
+
+import { TasksFilters } from '@challenge/shared';
+
+export class TaskFiltersQueryParam implements TasksFilters {
+  @ApiProperty({
+    example: 1,
+    description: 'Page number (starts from 1).',
+    default: 1,
+    required: false,
+  })
+  @IsNumber()
+  @Min(1)
+  @IsOptional()
+  @Transform(({ value: page }: { value: string }) => {
+    const numValue = page ? Number(page) : 1;
+
+    return Math.max(1, numValue);
+  })
+  page: number = 1;
+
+  @ApiProperty({
+    example: 10,
+    description: 'Number of items per page.',
+    default: 10,
+    required: false,
+  })
+  @IsNumber()
+  @Min(1)
+  @IsOptional()
+  @Transform(({ value: size }: { value: string }) => {
+    const numValue = size ? Number(size) : 10;
+
+    return Math.max(1, numValue);
+  })
+  size: number = 10;
+
+  @ApiProperty({
+    description: 'Filter tasks by status (comma separated)',
+    example: 'done,in_progress',
+    required: false,
+  })
+  @IsOptional()
+  @IsString()
+  status?: string;
+
+  @ApiProperty({
+    description: 'Filter tasks by priority (comma separated)',
+    example: 'high,low',
+    required: false,
+  })
+  @IsOptional()
+  @IsString()
+  priority?: string;
+}
