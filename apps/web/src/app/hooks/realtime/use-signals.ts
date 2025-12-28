959ecbec71cbd5d088d9a0879a53a750f699fd4c
oliveiradniel <kadadniel@gmail.com>
2025-12-28 09:18:20 -0300

    feat(web): add useSignals hook to handle task update and audit log signals

---
+++ apps/web/src/app/hooks/realtime/use-signals.ts
@@ -0,0 +1,32 @@
+import { useEffect } from 'react';
+import { useQueryClient } from '@tanstack/react-query';
+import { useMessaging } from './use-messaging';
+
+import { getNotificationsSocket } from '@/app/utils/notifications.socket';
+
+import { SOCKET_SIGNAL_KEYS } from '@challenge/shared';
+
+export function useSignals({ userId }: { userId?: string }) {
+  const queryClient = useQueryClient();
+
+  const { sinalizeTaskUpdated, sinalizeTaskAuditLog } = useMessaging({
+    queryClient,
+    userId,
+  });
+
+  const { TASK_UPDATED, TASK_AUDIT_LOG } = SOCKET_SIGNAL_KEYS;
+
+  useEffect(() => {
+    if (!userId) return;
+
+    const socket = getNotificationsSocket(userId);
+
+    socket.on(TASK_UPDATED, sinalizeTaskUpdated);
+    socket.on(TASK_AUDIT_LOG, sinalizeTaskAuditLog);
+
+    return () => {
+      socket.off(TASK_UPDATED, sinalizeTaskUpdated);
+      socket.off(TASK_AUDIT_LOG, sinalizeTaskAuditLog);
+    };
+  }, [userId]);
+}
