26f290ea56bf1ef8a7ecd2fd4058a08f05298f0d
oliveiradniel <kadadniel@gmail.com>
2025-12-30 13:18:45 -0300

    feat(web): create invalidateQueries helper

---
+++ apps/web/src/app/utils/invalidate-queries.ts
@@ -0,0 +1,40 @@
+import type { QueryClient } from '@tanstack/react-query';
+
+export type InvalidateQuery =
+  | { queryKey: ['task', taskId: string]; exact?: boolean }
+  | { queryKey: ['comments', taskId: string, page?: string]; exact?: boolean }
+  | { queryKey: ['task-creation-audit-logs']; exact?: boolean }
+  | { queryKey: ['task-deletion-audit-logs']; exact?: boolean }
+  | { queryKey: ['task-update-audit-logs']; exact?: boolean }
+  | {
+      queryKey: [
+        'tasks',
+        page?: string,
+        size?: string,
+        orderBy?: string,
+        order?: string,
+        status?: string,
+        priority?: string,
+        search?: string,
+      ];
+      exact?: boolean;
+    }
+  | { queryKey: ['unread-notifications']; exact?: boolean }
+  | { queryKey: ['read-notifications']; exact?: boolean }
+  | { queryKey: ['users-tasks', taskId: string]; exact?: boolean }
+  | { queryKey: ['users']; exact?: boolean }
+  | { queryKey: ['session']; exact?: boolean };
+
+interface InvalidateQueriesParams {
+  queryClient: QueryClient;
+  invalidateQuery: InvalidateQuery[];
+}
+
+export function invalidateQueries({
+  queryClient,
+  invalidateQuery,
+}: InvalidateQueriesParams) {
+  invalidateQuery.forEach(({ queryKey, exact = true }) => {
+    queryClient.invalidateQueries({ queryKey, exact });
+  });
+}
