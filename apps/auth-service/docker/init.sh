883cc82c5ba779e4a07b35cfe4fce3d270b0cb3e
oliveiradniel <kadadniel@gmail.com>
2026-01-07 17:32:02 -0300

    fix(docker): standardize database configuration and init scripts for local and container environments

--- apps/auth-service/docker/init.sh
+++ apps/auth-service/docker/init.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "Waiting for Postgres to start..."
 
-while ! pg_isready -h "$POSTGRES_HOST" -p "$POSTGRES_PORT" -U "$POSTGRES_USER" >/dev/null 2>&1; do
+while ! pg_isready -h "$DB_AUTH_SERVICE_HOST" -p "$DB_AUTH_SERVICE_PORT" -U "$DB_AUTH_SERVICE_USER" >/dev/null 2>&1; do
   echo "Database not ready yet, waiting..."
   sleep 2
 done
